04e7a45fc3ef4ed07c5931c16ac94e4da61def33
D H <46064123+CraicX@users.noreply.github.com>
2022-08-30 05:56:45 -0700

    Import to game fixed

---
+++ JTPoseDump_MOD/PoseClass.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace PoseDump
+{
+	public class PoseClass
+	{
+		private string _Name = "";
+		private int _Number = 0;
+		private float _Head = 0f;
+		private float _UpperBody = 0f;
+		private float _MiddleBody = 0f;
+		private float _LowerBody = 0f;
+		private float _UpperArm = 0f;
+		private float _UpperArmFront = 0f;
+		private float _LowerArm = 0f;
+		private float _LowerArmFront = 0f;
+		private float _UpperLeg = 0f;
+		private float _UpperLegFront = 0f;
+		private float _LowerLeg = 0f;
+		private float _LowerLegFront = 0f;
+		private float _Foot = 0f;
+		private float _FootFront = 0f;
+		private string _ConfigName = "";
+
+		public bool HasAngle(string limbName)  => (bool)(GetPropValue<float>(this, limbName) != 0f);
+		public float GetAngle(string limbName) => (float)GetPropValue<float>(this, limbName);
+
+		private bool _Animated = false, _ShouldStumble = false, _ShouldStandUpright = true;
+			
+		private float _Rigidity = 1.3f, _DragInfluence=0f, _UprightForceMultiplier = 2f, _AnimationSpeedMultiplier = 1.5f, _PoseRigidityModifier=0f;
+
+		private PoseStates _PoseState = PoseStates.Rest;
+		
+		public PoseStates PoseState
+		{
+			get { return _PoseState; }
+			set { _PoseState = value; }
+		}
+
+		public bool ShouldStandUpright
+		{
+			get { return _ShouldStandUpright; }
+			set { _ShouldStandUpright = value; }
+		}
+
+		public bool ShouldStumble
+		{
+			get { return _ShouldStumble; }
+			set { _ShouldStumble = value; }
+		}
+
+		public bool Animated
+		{
+			get { return _Animated; }
+			set { _Animated = value; }
+		}
+
+
+		public float UprightForceMultiplier
+		{
+			get { return _UprightForceMultiplier; }
+			set { _UprightForceMultiplier = value; }
+		}
+
+		public float AnimationSpeedMultiplier
+		{
+			get { return _AnimationSpeedMultiplier; }
+			set { _AnimationSpeedMultiplier = value; }
+		}
+
+		public float PoseRigidityModifier
+		{
+			get { return _PoseRigidityModifier; }
+			set { _PoseRigidityModifier = value; }
+		}
+
+		public float Rigidity
+		{
+			get { return _Rigidity; }
+			set { _Rigidity = value; }
+		}
+
+		public float DragInfluence
+		{
+			get { return _DragInfluence; }
+			set { _DragInfluence = value; }
+		}
+
+
+		
+		
+		public enum PoseStates
+		{
+			Rest,
+			Protective,
+			Flailing,
+			Stumbling,
+			Swimming,
+			WrithingInPain,
+			Walking,
+			Sitting,
+			Flat,
+			BrainDamage
+		}
+
+		public string Name
+		{
+			get { return _Name; }
+			set { _Name = value; }
+		}
+
+		public string ConfigName
+		{
+			get { return _ConfigName; }
+			set { _ConfigName = value; }
+		}
+
+		public int Number
+		{
+			get { return _Number; }
+			set { _Number = value; }
+		}
+
+		public float Head
+		{
+			get { return _Head; }
+			set { 
+				_Head = value; 
+			}
+		}
+
+		public float UpperBody
+		{
+			get { return _UpperBody; }
+			set { _UpperBody = value; }
+		}
+
+		public float MiddleBody
+		{
+			get { return _MiddleBody; }
+			set { _MiddleBody = value; }
+		}
+
+		public float LowerBody
+		{
+			get { return _LowerBody; }
+			set { _LowerBody = value; }
+		}
+
+		public float LowerArmFront
+		{
+			get { return _LowerArmFront; }
+			set { _LowerArmFront = value; }
+		}
+
+		public float UpperArmFront
+		{
+			get { return _UpperArmFront; }
+			set { _UpperArmFront = value; }
+		}
+
+		public float LowerArm
+		{
+			get { return _LowerArm; }
+			set { _LowerArm = value; }
+		}
+
+		public float UpperArm
+		{
+			get { return _UpperArm; }
+			set { _UpperArm = value; }
+		}
+
+		public float UpperLeg
+		{
+			get { return _UpperLeg; }
+			set { _UpperLeg = value; }
+		}
+
+		public float UpperLegFront
+		{
+			get { return _UpperLegFront; }
+			set { _UpperLegFront = value; }
+		}
+
+		public float LowerLeg
+		{
+			get { return _LowerLeg; }
+			set { _LowerLeg = value; }
+		}
+
+
+		public float LowerLegFront
+		{
+			get { return _LowerLegFront; }
+			set { _LowerLegFront = value; }
+		}
+
+		public float Foot
+		{
+			get { return _Foot; }
+			set { _Foot = value; }
+		}
+
+		public float FootFront
+		{
+			get { return _FootFront; }
+			set { _FootFront = value; }
+		}
+
+		
+		public Object GetPropValue(Object obj, String name) {
+			foreach (String part in name.Split('.')) {
+				if (obj == null) { return null; }
+
+				Type type = obj.GetType();
+				PropertyInfo info = type.GetProperty(part);
+				if (info == null) { return null; }
+
+				obj = info.GetValue(obj, null);
+			}
+			return obj;
+		}
+
+		public T GetPropValue<T>(Object obj, String name) {
+			Object retval = GetPropValue(obj, name);
+			if (retval == null) { return default(T); }
+
+			// throws InvalidCastException if types are incompatible
+			return (T) retval;
+		}
+	}
+
+}
--- JTPoseDump_MOD/PoseDump.cs
+++ JTPoseDump_MOD/PoseDump.cs
@@ -1,18 +1,9 @@
-﻿using System;
-using System.Collections;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Reflection;
-using System.Text;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.Experimental.Rendering;
-using UnityEditor.Experimental;
-
 using System.IO;
-using Newtonsoft.Json;
-
+using UnityEngine;
+using UnityEngine.Serialization;
 
 
 
@@ -20,6 +11,11 @@ namespace PoseDump
 {
 	public class PoseMain
 	{
+		public static void OnLoad()
+		{
+			BindKeys();
+		}
+
 		public static void Main()
 		{
 			ModAPI.Register<PoseDump>();
@@ -47,11 +43,11 @@ namespace PoseDump
 			{
 				string[] KeyParts = keyMapping.Split('>');
 
-				inputKey = "Dump-" + KeyParts[0].Trim();
+				inputKey = "JTPD-" + KeyParts[0].Trim();
 
 				if (!InputSystem.Has(inputKey))
 				{
-					ModAPI.RegisterInput("[Dump] " + KeyParts[1].Trim(), inputKey, (KeyCode)Enum.Parse(typeof(KeyCode), KeyParts[2].Trim()));
+					ModAPI.RegisterInput("[JTPD] " + KeyParts[1].Trim(), inputKey, (KeyCode)Enum.Parse(typeof(KeyCode), KeyParts[2].Trim()));
 
 					if (KeyParts.Length == 4)
 					{
@@ -67,7 +63,7 @@ namespace PoseDump
 				else
 				{
 					action = InputSystem.Actions[inputKey];
-					ModAPI.RegisterInput("[Dump] " + KeyParts[1].Trim(), inputKey, action.Key);
+					ModAPI.RegisterInput("[JTPD] " + KeyParts[1].Trim(), inputKey, action.Key);
 
 					InputAction actionNew  = InputSystem.Actions[inputKey];
 					actionNew.SecondaryKey = action.SecondaryKey;
@@ -82,6 +78,24 @@ namespace PoseDump
 		public static string posePath = "poses/";
 		public static string poseFile = posePath + "poseFile.json";
 		public static float ThumbnailPadding = 1.5f;
+		public static RagdollPose Ragdoll;
+		public static PoseConfig PConfig;
+		public static PoseClass poseClass;
+
+		public static int poseID;
+
+		public static Dictionary<string, LimbMove> moves = new Dictionary<string, LimbMove>();
+
+
+
+		public void Update()
+		{
+			if (PoseMain.KeyCheck("JTPD-dumpPose")) DumpPose(GetSelected());
+			if (PoseMain.KeyCheck("JTPD-doPose")) ActivatePose(GetSelected());
+		}
+
+		static PoseConfig GetConfig( string configName ) => ( PoseConfigs.TryGetValue( configName, out PoseConfig config ) ) 
+															? config : new PoseConfig();
 
 		public static void DumpPose(List<PhysicalBehaviour> PBS)
 		{
@@ -145,21 +159,246 @@ namespace PoseDump
 
 			ContraptionSerialiser.SaveThumbnail(states, "poserDump");
 
+			ModAPI.Notify("Dumped Pose!");
+
 		}
 
-		public static void ActivatePose()
+		public static void ActivatePose(List<PhysicalBehaviour> PBS)
 		{
-			string poseFile = "C:\\pp\\poses\\export.txt";
+			string jsonFile = "C:\\pp\\poses\\export.txt.json";
 
-			if (!File.Exists(poseFile)) return;
+			if (!File.Exists(jsonFile)) return;
+			ModAPI.Notify("found file");
+			string jsonSource = File.ReadAllText(jsonFile);
 
-			string poseText = File.ReadAllText(poseFile);
+			PoseClass poseClass = JsonConvert.DeserializeObject<PoseClass>(jsonSource);
 
+			List<PersonBehaviour> People = new List<PersonBehaviour>();
 
+			foreach (PhysicalBehaviour PB in PBS)
+			{
+				PersonBehaviour person = PB.gameObject.GetComponentInParent<PersonBehaviour>();
 
+				if (!person || People.Contains(person)) continue; 
+
+				People.Add(person);
+			}
+
+			foreach ( PersonBehaviour PBO in People )
+			{
+				ImportPose(PBO, poseClass);
+
+				PBO.OverridePoseIndex = poseID;
+			}
 
 		}
 
+		public static void ImportPose( PersonBehaviour PBO, PoseClass PoseData )
+		{
+			string poseName = "JTPD_" + PoseData.GetHashCode().ToString();
+			
+			if (PoseData.ConfigName != "") {
+
+				PConfig = GetConfig( PoseData.ConfigName );
+
+			}
+
+			PConfig.Rigidity = PoseData.Rigidity;
+
+
+			Ragdoll   = new RagdollPose()
+			{
+				Name                     = poseName,
+				Rigidity                 = PConfig.Rigidity,
+				ShouldStandUpright       = PConfig.ShouldStandUpright,
+				DragInfluence            = PConfig.DragInfluence,
+				UprightForceMultiplier   = PConfig.UprightForceMultiplier,
+				ShouldStumble            = PConfig.ShouldStumble,
+				State                    = PConfig.State,
+				AnimationSpeedMultiplier = PConfig.AnimationSpeedMultiplier,
+				Angles                   = new List<RagdollPose.LimbPose>(),
+			};
+
+			RagdollPose.LimbPose limbPose;
+
+			foreach (LimbBehaviour limb in PBO.Limbs)
+			{
+				if (limb.HasJoint && PoseData.HasAngle(limb.name))
+				{
+					limbPose = new RagdollPose.LimbPose(limb, PoseData.GetAngle(limb.name))
+					{
+						Name                 = poseName,
+						Limb                 = limb,
+						Animated             = PConfig.Animated,
+						PoseRigidityModifier = PConfig.PoseRigidityModifier,
+						StartAngle           = 0f,
+						EndAngle             = 0f,
+						AnimationDuration    = 0f,
+						RandomInfluence      = PConfig.RandomInfluence,
+						RandomSpeed          = PConfig.RandomSpeed,
+						TimeOffset           = PConfig.TimeOffset,
+						AnimationCurve       = new AnimationCurve(),
+					};
+				}
+				else 
+				{
+					limbPose = new RagdollPose.LimbPose(limb,0f)
+					{
+						Name                 = poseName,
+						Limb                 = limb,
+						Animated             = false,
+						PoseRigidityModifier = 0f,
+						Angle                = 0f,
+						StartAngle           = 0f,
+						EndAngle             = 0f,
+						AnimationDuration    = 0f,
+						RandomInfluence      = 0f,
+						RandomSpeed          = 0f,
+						TimeOffset           = 0f,
+						AnimationCurve       = new AnimationCurve(),
+					};
+				}
+				Ragdoll.Angles.Add(limbPose);
+			}
+
+			Ragdoll.ConstructDictionary();
+
+			for (int pnum = PBO.Poses.Count; --pnum >= 0;)
+			{
+				if (PBO.Poses[pnum].Name == poseName)
+				{
+					poseID = pnum;
+
+					PBO.Poses[pnum] = Ragdoll;
+
+					return;
+				}
+			}
+
+			PBO.Poses.Add(Ragdoll);
+
+			poseID = PBO.Poses.Count -1;
+
+			ModAPI.Notify("Pose Activated: " + PoseData.Name);
+		}
+
+		public static List<PhysicalBehaviour> GetSelected(bool bothTypes = false)
+		{
+			List<PhysicalBehaviour> PBL = new List<PhysicalBehaviour>();
+
+			if (bothTypes || SelectionController.Main.SelectedObjects.Count == 0)
+			{
+				foreach (PhysicalBehaviour PB in Global.main.PhysicalObjectsInWorld)
+				{
+					if (!PB.Selectable || PB.colliders.Length == 0) continue;
+
+					foreach (Collider2D collider in PB.colliders)
+					{
+						if (!collider) continue;
+
+						if (collider.OverlapPoint((Vector2)Global.main.MousePosition))
+						{
+							if (PB.transform.root.TryGetComponent<PhysicalBehaviour>(out PhysicalBehaviour component))
+							{
+								if (component.Selectable) PBL.Add(component);
+							}
+							else
+							{
+								PBL.Add(PB);
+							}
+						}
+					}
+				}
+			}
+
+			if (bothTypes || PBL.Count == 0) PBL.AddRange(SelectionController.Main.SelectedObjects);
+
+			return PBL;
+
+		}
+
+		public struct PoseConfig
+		{
+			public string Name;
+		
+			public bool AutoStart, Animated, ShouldStumble, ShouldStandUpright, EnableArmAim, ComboMove;
+			
+			public float Rigidity, DragInfluence, UprightForceMultiplier, AnimationSpeedMultiplier, PoseRigidityModifier, 
+						 StartAngle, EndAngle, AnimationDuration, RandomSpeed, RandomInfluence, TimeOffset, Force;
+
+			public PoseState State;
+			public RagdollPose PresetPose;
+
+			public PoseConfig( string _name )
+			{
+				Name = _name;
+				AutoStart = Animated = EnableArmAim = ComboMove = false;
+				ShouldStumble = ShouldStandUpright = true;
+				Rigidity = DragInfluence = UprightForceMultiplier = AnimationSpeedMultiplier = AnimationDuration = 0f;
+				PoseRigidityModifier = StartAngle = EndAngle = RandomSpeed = RandomInfluence = TimeOffset = 0f;
+
+				Force = 4f;
+
+				State = PoseState.Rest;
+				PresetPose = null;
+			}
+		}
+
+		static Dictionary<string, PoseConfig> PoseConfigs = new Dictionary<string, PoseConfig>()
+		{
+			{ "Common", new PoseConfig() {
+				State                    = PoseState.Rest,
+				ShouldStandUpright		 = true,
+				Rigidity                 = 1.3f,
+				AnimationSpeedMultiplier = 1.5f,
+				UprightForceMultiplier   = 2f,
+				AutoStart                = true,
+			} },
+			{ "CommonDuck", new PoseConfig() {
+				ShouldStandUpright       = true,
+				State                    = PoseState.Rest,
+				Rigidity                 = 1.7f,
+				AnimationSpeedMultiplier = 2.5f,
+				UprightForceMultiplier   = 1.4f,
+				AutoStart                = true,
+			} },
+			{ "CommonDown", new PoseConfig() {
+				ShouldStandUpright       = false,
+				State                    = PoseState.Rest,
+				Rigidity                 = 2.3f,
+				AnimationSpeedMultiplier = 2.5f,
+				UprightForceMultiplier   = 0f,
+				AutoStart                = true,
+			} },
+			{ "NoAuto", new PoseConfig() {
+				ShouldStandUpright       = false,
+				State                    = PoseState.Rest,
+				Rigidity                 = 2.3f,
+				AnimationSpeedMultiplier = 2.5f,
+				UprightForceMultiplier   = 4f,
+				AutoStart                = true,
+			} },
+			{ "Combo", new PoseConfig() {
+				ShouldStandUpright       = true,
+				State                    = PoseState.Rest,
+				ComboMove                = false,
+				AutoStart                = true,
+			} },
+			{ "Gesture", new PoseConfig() {
+				State                    = PoseState.Rest,
+				ShouldStandUpright		 = true,
+				Rigidity                 = 1.5f,
+				AnimationSpeedMultiplier = 1f,
+				UprightForceMultiplier   = 4f,
+				AutoStart                = true,
+			} },
+		};
+
+		public struct LimbMove
+		{
+			public string limbName;
+			public float angle, rigid, force;
+		}
 	}
 
 
